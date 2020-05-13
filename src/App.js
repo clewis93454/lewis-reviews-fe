f76ba4e27880eb228ad7f932cb02fbd583f4273b
Curtis Lewis <lewis.curtis.t@gmail.com>
2020-05-12 20:53:30 -0600

    stubbed out basic structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <body>This is my test body</body>
+      <Header />
+      <main>
+        <Router>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/about">
+            <AboutPage />
+          </Route>
+          <Route path="/contact">
+            <ContactPage />
+          </Route>
+        </Router>
+      </main>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
